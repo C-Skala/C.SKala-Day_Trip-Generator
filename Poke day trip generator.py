10a8d0a15d5e18d379765e1cf4bc8121a5fa8dfd
C-Skala <cjskala@gmail.com>
2022-06-01 14:49:31 -0400

    added the names for the lists that will be needed

--- Poke day trip generator.py
+++ Poke day trip generator.py
@@ -3,26 +3,49 @@ print("Hello and welcome to the wonderful world of pokemon, lets get you all set
 import random
 places_to_visit = ["Kanto", "Jhoto", "Hoenn", "Sinnoh", "Aloha", "Galar"]
 
-def destination_picker():
+def region_picker():
     location_picked = False
     poke_places = random.choice(places_to_visit)
     travel_location = input('Would you like to visit the region of ' + f'{poke_places}? ' + 'Please select y/n ')
     if travel_location == 'y':
         location_picked = True
-        print('Thats a great region, lets figure out the details now!')
         return poke_places
     while location_picked == False:
         poke_places = random.choice(places_to_visit)
         travel_location = input('I get it, that region is not for everyone. What about ' + f'{poke_places}? ' + 'Please select y/n ')
         if travel_location == 'y':
             location_picked = True
-            print('Thats a great region, lets figure out the details now!')
+            
             return poke_places
 
-picked_destination = destination_picker()
-print(picked_destination)
+picked_region = region_picker()
+print(picked_region)
+print('Thats a great region, lets figure out the details now!')
 
-Kato_food = ['']
+Kanto_food = ['']
+Kanto_transport = ['']
+kanto_entertainment = ['']
+
+Jhoto_food = ['']
+Jhoto_transport = ['']
+Jhoto_entertainment = ['']
+
+Hoenn_food = ['']
+Hoenn_transport = ['']
+Hoenn_entertainment = ['']
+
+Sinnoh_food = ['']
+Sinnoh_transport = ['']
+Sinnoh_entertainment = ['']
+
+Aloha_food = ['']
+Aloha_transport = ['']
+Aloha_entertainment = ['']
+
+Galar_food = ['']
+Galar_transport = ['']
+Galar_entertainment = ['']
+
 
 def entertainment_picker(destinaiton):
     if destinaiton == "Kanto":
